4457c60caf211f68dd582e644beada94218d16e0
scotthbran <brandon.scotthill@publons.com>
2020-01-27 16:20:23 +1300

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='auto_behave',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    long_description=README,
+    description=(
+        'A sphinx extension for behave to auto document'
+    ),
+    install_requires=[
+        'sphinx',
+        'docutils',
+        'jinja2',
+    ],
+    url='https://github.com/publons/auto_behave',
+    author='Brandon Scott-Hill',
+    author_email='brandon.scotthill@publons.com',
+    classifiers=[
+        'Framework:: Sphinx:: Extension'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Documentation :: Sphinx'
+    ],
+    python_requires='>=3.6',
+)
